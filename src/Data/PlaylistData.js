b19d469442ad124481897c33d7990368571fdf06
Ryan Shee <ryan.shee@gmail.com>
2021-08-22 21:59:43 -0700

    Update playlists

--- src/Data/PlaylistData.js
+++ src/Data/PlaylistData.js
@@ -6,18 +6,20 @@ module.exports = {
 			"https://www.youtube.com/watch?v=5qap5aO4i9A",
 			"https://www.youtube.com/watch?v=5yx6BWlEVcY",
 			"https://www.youtube.com/watch?v=-5KAN9_CzSA",
-			"https://www.youtube.com/watch?v=Q326lVMgsHk",
-			"https://www.youtube.com/watch?v=-5KAN9_CzSA",
-			"https://www.youtube.com/watch?v=Xry_g0Vp5MY",
-			"https://www.youtube.com/watch?v=1NCOFZpZE2Q",
+			"https://www.youtube.com/watch?v=VfW86fnQL5w",
+			"https://www.youtube.com/watch?v=21qNxnCS8WU",
+			"https://www.youtube.com/playlist?list=PL5FeYIiqkJGoJPMzA1UACc40G-tP29Yqe",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGpbQSM1par2UH7oDh6Hy_Ny",
 		]
 	},
 	kpop: {
 		label: "Korean",
 		value: "kpop",
 		playlists: [
-			"https://www.youtube.com/watch?v=QNS3SXYyaUY&list=PL5FeYIiqkJGrIHcXmkcUvQUCHN-h3eyay",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGrIHcXmkcUvQUCHN-h3eyay",
 			"https://www.youtube.com/watch?v=lKlSesz-V-8",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGrnMju-F4cL8wYmme31a5ko",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGq5yBy2MmxZDV5_qEGN5EjD",
 			"https://www.youtube.com/watch?v=ntOYV6mz_7Q",
 		]
 	},
@@ -25,34 +27,36 @@ module.exports = {
 		label: "Japanese",
 		value: "jpop",
 		playlists: [
-			"https://www.youtube.com/watch?v=1gSe78TIEEk",
+			"https://www.youtube.com/watch?v=pbqbnSQ5pxw",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGp3zDpuMalDz6Ckl2Hss38c",
 			"https://www.youtube.com/watch?v=DXHgBUMnlvY",
-			"https://www.youtube.com/watch?v=9FvvbVI5rYA",
 			"https://www.youtube.com/watch?v=WHUhgVz2JDg",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGrdULL5qlPIqjmeY7J2_tQl",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGpuG8xezGAPRJXskN0fWQTq",
 		]
 	},
 	anime: {
 		label: "Animation",
 		value: "anime",
 		playlists: [
-			"https://www.youtube.com/watch?v=UoMbwCoJTYM",
-			"https://www.youtube.com/watch?v=SbHhaVmdEvc",
-			"https://www.youtube.com/watch?v=zyAdlInC_P0",
-			"https://www.youtube.com/watch?v=zcaskjhhXWQ",
-			"https://www.youtube.com/watch?v=QE239OnSAu4&list=PL5FeYIiqkJGo3f96VYm4nkJXQZOXLV7Bv",
-			"https://www.youtube.com/watch?v=OEtx1ek3-h8",
+			"https://www.youtube.com/watch?v=NAbQimfaRyw",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGo3f96VYm4nkJXQZOXLV7Bv",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGqxzZ3WCbsw0P__gNN2a7K7",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGqMqAf8o-_Qh60WZ25VkZCb",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGqsW5C3vMfFyjLN-NoTRWLd",
 		]
 	},
 	games: {
 		label: "Games",
 		value: "games",
 		playlists: [
-			"https://www.youtube.com/watch?v=wofB1wzyYYI",
-			"https://www.youtube.com/watch?v=GdzrrWA8e7A",
-			"https://www.youtube.com/watch?v=WfMClt3K5K4",
-			"https://www.youtube.com/watch?v=MO30O_f1nzU",
-			"https://www.youtube.com/watch?v=SlUYv-CUoOo&list=PL9Xuki_HcjmBJPp_ku7MHmJke1jtc_QTq",
-			"https://www.youtube.com/watch?v=7JMvn0wfABQ",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGrwyUzXGxXhjaj2012wM4H0",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGo2jJiUSJcWJW-Lz8OUZWzI",
+			"https://youtube.com/playlist?list=PL9Xuki_HcjmBJPp_ku7MHmJke1jtc_QTq",
+			"https://youtube.com/playlist?list=PL9kznmoYOdz-wp4qZiYgffZjU59zTakpN",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGq_aDpRcEJt7JNAEAPeAqQl",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGpgmiIeYDYuVbySMlNNDMnt",
+			"https://youtube.com/playlist?list=PL5FeYIiqkJGqJJaK2YDr3MO7CHrshptAf",
 		]
 	},
 	own: {
