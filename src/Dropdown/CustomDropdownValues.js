3e8a459686c4bd6db0adb1f273f470f59ddc7fe1
Ryan Shee <ryan.shee@gmail.com>
2021-09-01 20:38:31 -0700

    #1: Cheated hover property for react-dropdown, fix dropdown border radius

--- src/Dropdown/CustomDropdownValues.js
+++ src/Dropdown/CustomDropdownValues.js
@@ -29,7 +29,7 @@ module.exports = {
 			border: `2px solid ${lightPurpleFade}`,
 			padding: '0',
 			background: navy,
-			borderRadius: '6px',
+			borderRadius: '9px',
 			maxHeight: '100%',
 			overflow: 'hidden'
 		},
